88555c5329184f95481cae6ff397dfc8511fe8a0
Heath Boutilier <heathboutilier@outlook.com>
2025-10-01 11:08:18 -0700

    C1L4: REPL

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
-func main(){
-	fmt.Println("Hello, World!")
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	for input.Scan() {
+		args := cleanInput(input.Text())
+		if args[0] == "q" {
+			fmt.Println("Exiting...")
+			break
+		}
+		fmt.Printf("Command entered: %s\n", args[0])
+		fmt.Print("Pokedex > ")
+	}
+}
+
+func cleanInput(text string) []string {
+	operableText := strings.ToLower(text)
+	stringSlice := strings.Split(operableText, " ")
+
+	var rv []string
+	for _, val := range stringSlice {
+		if val != "" {
+			rv = append(rv, val)
+		}
+	}
+	return rv
 }
