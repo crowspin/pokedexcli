754396a268182363702d4631ed7bd12a6837ef0e
Heath Boutilier <heathboutilier@outlook.com>
2025-10-01 11:32:56 -0700

    C1L5: Help + Exit

--- main.go
+++ main.go
@@ -7,18 +7,53 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var CommandRegistry map[string]cliCommand
+
 func main() {
-	input := bufio.NewScanner(os.Stdin)
-	fmt.Print("Pokedex > ")
-	for input.Scan() {
-		args := cleanInput(input.Text())
-		if args[0] == "q" {
-			fmt.Println("Exiting...")
-			break
-		}
-		fmt.Printf("Command entered: %s\n", args[0])
-		fmt.Print("Pokedex > ")
+	CommandRegistry = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
 	}
+
+	input := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		input.Scan()
+		args := cleanInput(input.Text())
+		if cmd, ok := CommandRegistry[args[0]]; ok {
+			cmd.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, v := range CommandRegistry {
+		fmt.Printf("%s: %s\n", v.name, v.description)
+	}
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
 
 func cleanInput(text string) []string {
